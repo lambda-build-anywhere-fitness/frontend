adc837c9eca06bd333c5513c5033643c3af8e7d9
JoshuaHolloway <jhollow6@asu.edu>
2021-05-19 16:24:58 -0500

    rough draft of form component

--- components/forms/registration-form/registration-form.js
+++ components/forms/registration-form/registration-form.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import '../../../global-styles/styles.scss';
+
+// ==============================================
+// ==============================================
 
 const FormContainer = styled.div`
   display: flex;
@@ -8,19 +10,105 @@ const FormContainer = styled.div`
   align-items: center;
   height: 100vh;
   width: 100vw;
-  border: solid red 5px;
-`; // FormContaienr ``
+  background: linear-gradient(90deg, var(--gradient-starting), var(--gradient-ending));
+  color: var(--text-primary);
+`; // FormContainer ``
+
+// ==============================================
+// ==============================================
 
 const Form = styled.form`
-  
-  height: 100px;
-  border: dashed green 5px;
+  display: grid;
+  grid-template-columns: 1fr;  
+  grid-template-rows: repeat(5, 1fr);
+  height: 700px;
+  width: 400px;
+  /* border: dashed green 5px; */
 `; // Form ``
 
+// ==============================================
+// ==============================================
+
+const FormRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  text-align: center;
+  /* border: dotted purple 5px; */
+
+  .top, .bottom {
+    text-align: center;
+    /* border: solid white 2px; */
+
+    &.form-input {
+      text-align: left;
+      padding: 0 2%;
+      border: 0;
+      border-bottom: solid var(--translucent-primary) 2px;
+    }
+  }
+`;
+
+// ==============================================
+// ==============================================
+
+const Button = styled.button`
+  height: 60px;
+  width: 100%;
+  border-radius: 5px;
+  border: none;
+  color: var(--text-primary);
+  background: var(--translucent-primary);
+  transition: box-shadow 0.3s ease, transform 0.3s ease;
+  &:hover { 
+    box-shadow: var(--hover-shadow);
+    transform: scaleX(1.01) scaleY(1.01);
+  }
+`;
+
+// ==============================================
+// ==============================================
+
+// ==============================================
+// ==============================================
+
+const Solid = styled.span`
+`;
+const Translucent = styled.span`
+  color: var(--translucent-primary);
+`;
 const RegistrationForm = () => {
   return (
     <FormContainer>
-      <Form>Registration Form</Form>
+      <Form>
+        <FormRow>
+          <div className="top">
+            <h4>CREATE YOUR APP ACCOUNT</h4>
+          </div>
+          <div className="bottom">
+            <Solid>1</Solid> <Solid>&#8212;</Solid> <Translucent>2</Translucent> <Translucent>&#8212;</Translucent> <Translucent>3</Translucent>
+          </div>
+        </FormRow>
+        <FormRow>
+          <h3>New to the app? Let's create your login!</h3>
+        </FormRow>
+        <FormRow>
+          <div className="top form-input">
+            email
+            {/* TODO: Place a text input field for email here */}
+          </div>
+          <div className="bottom form-input">
+            password
+            {/* TODO: Place a text input field for password here */}
+          </div>
+        </FormRow>
+        <FormRow>
+          {/* Can display form error messages here */}
+        </FormRow>
+        <FormRow>
+          <Button>NEXT</Button>
+        </FormRow>
+      </Form>
     </FormContainer>
   );
 }
