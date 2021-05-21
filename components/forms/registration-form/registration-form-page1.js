1d90c31b148c5833c382493309da53a805339bb1
JoshuaHolloway <jhollow6@asu.edu>
2021-05-21 13:25:12 -0500

    added gsap

--- components/forms/registration-form/registration-form-page1.js
+++ components/forms/registration-form/registration-form-page1.js
@@ -12,12 +12,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const buttonStyles = makeStyles((theme) => ({
   root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 'dashed white 5px',
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
+
     padding: '0 30px',
 
     height: '60px',
