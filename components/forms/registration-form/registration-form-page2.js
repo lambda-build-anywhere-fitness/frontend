868aa4a0ac4913ed8dd48af5aac1696f9ce0ff95
JoshuaHolloway <jhollow6@asu.edu>
2021-05-21 16:41:10 -0500

    update

--- components/forms/registration-form/registration-form-page2.js
+++ components/forms/registration-form/registration-form-page2.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+// import Switch from '@material-ui/core/Switch';
+import Checkbox from '@material-ui/core/Checkbox';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -114,8 +116,6 @@ const RegistrationFormPg1 = ({setFormData}) => {
   // --------------------------------------------
 
   useEffect(() => {
-    
-    // animate:  "2 -"
     const timeline = gsap.timeline();
     timeline.to([translucent1Ref.current, translucent2Ref.current], { 
       color: 'white',
@@ -129,7 +129,6 @@ const RegistrationFormPg1 = ({setFormData}) => {
       scale: 2.1,
       duration: 0.3,
     });
-
     timeline.from([input1Ref.current, input2Ref.current, input3Ref.current], {
       x: '-100vw', 
       ease: "power2.out",
@@ -154,12 +153,19 @@ const RegistrationFormPg1 = ({setFormData}) => {
   // --------------------------------------------
   const init_form = { first_name: '', last_name: '', country: '', mailing_list: false };
   const [form, setForm] = useState(init_form);
+  const [checked, setChecked] = useState(true);
+
+  // --------------------------------------------
+
+  const toggleCheckbox = event => {
+    setChecked(event.target.checked);
+    console.log(event.target);
+  };
 
   // --------------------------------------------
 
   const onChange = (event) => {
-    console.log('onChange() -- form: ', form);
-    const { name, value } = event.target;
+    const { name, value, type, checked } = event.target;
     setForm( {...form, [name]: value} );
   };
   
@@ -169,29 +175,23 @@ const RegistrationFormPg1 = ({setFormData}) => {
   const onPost = (event) => {
     console.log('onPost() in registration-form component');
     event.preventDefault();
-
     const formData = {
       "first_name": `${form.first_name}`,
       "last_name": `${form.last_name}`,
-      "country": `${form.country}`,
+      "mailing_list": false,
     };
     setFormData(formData);
-
     const animate_page_transition_during_post_request = (() => {
       const progress_bar = document.querySelector('#registrationFormPg1__LinearProgress');
       progress_bar.classList.toggle('hide-visibility');
-
-      const duration = 2.5;
       gsap.to([input1Ref.current, input2Ref.current, input3Ref.current], {
         x: '100vw', 
-        delay: duration - 0.5, 
+        delay: 2, 
         ease: "power2.out",
         stagger: 0.15,
       });
       setTimeout(() => history.push("/registration-page-3"), duration * 1e3);
-
     })();
-
     // axios.post('http://localhost:5000/x', user)
     //   .then((response) => {
     //     console.log(response.data);
@@ -233,13 +233,36 @@ const RegistrationFormPg1 = ({setFormData}) => {
             />
           </div>
           <div className="bottom form-input">
-            <TextField id="standard-basic" label="country"
+            {/* <TextField id="standard-basic" label="country"
               name="country" 
               value={form.country} 
               onChange={onChange}
               className={inputClasses.root}
               ref={input3Ref}
-            />
+            /> */}
+
+            <div className="switch-container" ref={input3Ref}>
+              {/* <Switch 
+                checked={form.mailing_list}
+                onChange={onChange}
+                name="mailing_list" 
+              ></Switch> */}
+
+              {/* <Checkbox
+                checked={form.mailing_list}
+                onChange={onChange}
+                name="mailing_list" 
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              /> */}
+              Get Emails with news and promo?
+              <Checkbox
+                checked={false}
+                onChange={toggleCheckbox}
+                name="mailing_list" 
+              />
+              
+            </div>
+
           </div>
         </FormRow>
 
--- components/forms/registration-form/registration-form-page3.js
+++ components/forms/registration-form/registration-form-page3.js
@@ -26,8 +26,7 @@ const RegistrationFormPg3 = ({formData}) => {
       <p>Form <strong>Password</strong> Field (in State): {formData.password}</p>
       <p>Form <strong>First Name</strong> Field (in State): {formData.first_name}</p>
       <p>Form <strong>Last Name</strong> Field (in State): {formData.last_name}</p>
-      <p>Form <strong>Country</strong> Field (in State): {formData.country}</p>
-      <p>Form <strong>Mailing List?</strong> Field (in State): {formData.mailing_list}</p>
+      <p>Form <strong>Mailing List?</strong> Field (in State): {String(formData.mailing_list)}</p>
 
       <p>All the combined form data is sent to the server on the previous page (hence the loading animation).  This is done in line 195 in 'registration-form-page2.js'</p>
 
